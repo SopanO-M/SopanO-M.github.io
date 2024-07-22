9dc4829d71da76290d1419eff3ef5ae112973049
SopanO-M <sopantechnical@gmail.com>
2024-07-22 17:48:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,23 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(showSlides, 4000); // Change image every 4 seconds
     }
 
-        // Add event listeners to buttons for redirection
-        document.getElementById('simpleButton').addEventListener('click', function() {
-            window.location.href = 'https://drive.google.com/file/d/13TVe6_PeAjjCftrf0_fteITsGTChHEqM/view?usp=drive_link';
-        });
-    
-        document.getElementById('rightButton').addEventListener('click', function() {
-            window.location.href = 'https://drive.google.com/file/d/13Qvw0DxFoyZql5bHZgHTAIc7rueoKN2_/view?usp=drive_link';
-        });
-
-        
-
-    // Hide preloader after page load
-    const preloader = document.querySelector('.preloader');
-    window.addEventListener('load', () => {
-        setTimeout(() => {
-            preloader.style.display = 'none';
-            showSlides(); // Start the slider animation after the preloader hides
-        }, 1000); // Keep the preloader visible for at least 3 seconds
+    // Add event listeners to buttons for redirection
+    document.getElementById('simpleButton').addEventListener('click', function() {
+        window.location.href = 'https://drive.google.com/file/d/13TVe6_PeAjjCftrf0_fteITsGTChHEqM/view?usp=drive_link';
     });
+    
+    document.getElementById('rightButton').addEventListener('click', function() {
+        window.location.href = 'https://drive.google.com/file/d/13Qvw0DxFoyZql5bHZgHTAIc7rueoKN2_/view?usp=drive_link';
+    });
+
+    // Start the slider animation immediately
+    showSlides(); 
 });
